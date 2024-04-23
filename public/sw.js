9743e6cb7bc4c9a4a9cb5dea3d4791cd91d9f1b4
man2502 <man.tlf25@gmail.com>
2024-04-23 18:40:32 +0500

    add service worker to index html

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+// import { store } from "../src/store/store";
+// service-worker.js
+const CACHE_NAME = 'video-cache-v1';
+
+// const videos = store.getState().video.videos
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(videos);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    response => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
